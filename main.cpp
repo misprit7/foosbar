fe4266d221b6bb3a7d8f076f88dcf08e1e9252ae
misprit7 <xandernaumenko@gmail.com>
2023-10-02 22:34:53 -0700

    Added homing

--- main.cpp
+++ main.cpp
@@ -14,9 +14,11 @@ using namespace cv;
 using namespace sFnd;
 
 
-#define ACC_LIM_RPM_PER_SEC	10000
-#define VEL_LIM_RPM			100
-#define MOVE_DISTANCE_CNTS	100	
+#define ACC_LIM_RPM_PER_SEC    10000
+#define VEL_LIM_RPM            200
+#define MOVE_DISTANCE_CNTS     -2300    
+
+#define HOMING_TIMEOUT 10000
 
 
 int main( int argc, char** argv ){
@@ -24,84 +26,100 @@ int main( int argc, char** argv ){
     SysManager cp_mgr;
     std::vector<std::string> comHubPorts;
 
-	try
-	{ 
-		// Identify hubs
-		SysManager::FindComHubPorts(comHubPorts);
-		printf("Found %zu SC Hubs\n", comHubPorts.size());
+    try
+    { 
+        // Identify hubs
+        SysManager::FindComHubPorts(comHubPorts);
+        printf("Found %zu SC Hubs\n", comHubPorts.size());
 
-		// Find available ports
+        // Find available ports
         size_t portCount = 0;
-		for (portCount = 0; portCount < comHubPorts.size() && portCount < NET_CONTROLLER_MAX; portCount++) {
-			cp_mgr.ComHubPort(portCount, comHubPorts[portCount].c_str());
-		}
+        for (portCount = 0; portCount < comHubPorts.size() && portCount < NET_CONTROLLER_MAX; portCount++) {
+            cp_mgr.ComHubPort(portCount, comHubPorts[portCount].c_str());
+        }
 
-		if (portCount < 0) {
-			printf("Unable to locate SC hub port\n");
-			return -1;
-		}
+        if (portCount < 0) {
+            printf("Unable to locate SC hub port\n");
+            return -1;
+        }
 
-		// Open ports
-		cp_mgr.PortsOpen(portCount);
+        // Open ports (hubs)
+        cp_mgr.PortsOpen(portCount);
 
         IPort &hub0 = cp_mgr.Ports(0);
-		printf(" Port[%d]: state=%d, nodes=%d\n",
-			hub0.NetNumber(), hub0.OpenState(), hub0.NodeCount());
+        printf(" Port[%d]: state=%d, nodes=%d\n",
+            hub0.NetNumber(), hub0.OpenState(), hub0.NodeCount());
 
-		INode &linear_node = hub0.Nodes(0);
-		linear_node.EnableReq(false);
-		cp_mgr.Delay(200);
+        // Open nodes (motors)
+        INode &linear_node = hub0.Nodes(0);
+        linear_node.EnableReq(false);
+        cp_mgr.Delay(200);
 
-		printf("   Node[%d]: type=%d\n", 0, linear_node.Info.NodeType());
-		printf("            userID: %s\n", linear_node.Info.UserID.Value());
-		printf("        FW version: %s\n", linear_node.Info.FirmwareVersion.Value());
-		printf("          Serial #: %d\n", linear_node.Info.SerialNumber.Value());
-		printf("             Model: %s\n", linear_node.Info.Model.Value());
+        printf("   Node[%d]: type=%d\n", 0, linear_node.Info.NodeType());
+        printf("            userID: %s\n", linear_node.Info.UserID.Value());
+        printf("        FW version: %s\n", linear_node.Info.FirmwareVersion.Value());
+        printf("          Serial #: %d\n", linear_node.Info.SerialNumber.Value());
+        printf("             Model: %s\n", linear_node.Info.Model.Value());
+
+        // Enable motor
+        linear_node.Status.AlertsClear();
+        linear_node.Motion.NodeStopClear();
+        linear_node.EnableReq(true);
+        printf("Node \t%i enabled\n", 0);
+
+        double timeout = cp_mgr.TimeStampMsec() + 2000;
+        while (!linear_node.Motion.IsReady()) {
+            if (cp_mgr.TimeStampMsec() > timeout) {
+                printf("Error: Timed out waiting for Node %d to enable\n", 0);
+                return -2;
+            }
+        }
+
+        // Homing
+        if (linear_node.Motion.Homing.HomingValid()){
+            linear_node.Motion.Homing.Initiate();
+
+            timeout = cp_mgr.TimeStampMsec() + HOMING_TIMEOUT;            
+
+            while (!linear_node.Motion.Homing.WasHomed()) {
+                if (cp_mgr.TimeStampMsec() > timeout) {
+                    printf("Homing failed!\n");
+                    return -2;
+                }
+            }
+        }
+
+        // Set motion parameters
+        linear_node.Motion.MoveWentDone(); // Clear move done register
+        linear_node.AccUnit(INode::RPM_PER_SEC);
+        linear_node.VelUnit(INode::RPM);
+        linear_node.Motion.AccLimit = ACC_LIM_RPM_PER_SEC;
+        linear_node.Motion.VelLimit = VEL_LIM_RPM;
+
+        // Send move
+        printf("Moving Node \t%i \n", 0);
+        linear_node.Motion.MovePosnStart(MOVE_DISTANCE_CNTS);
+        printf("%f estimated time.\n", linear_node.Motion.MovePosnDurationMsec(MOVE_DISTANCE_CNTS));
+        timeout = cp_mgr.TimeStampMsec() + linear_node.Motion.MovePosnDurationMsec(MOVE_DISTANCE_CNTS) + 100;
+
+        while (!linear_node.Motion.MoveIsDone()) {
+            if (cp_mgr.TimeStampMsec() > timeout) {
+                printf("Error: Timed out waiting for move to complete\n");
+                return -2;
+            }
+        }
+        printf("Node \t%i Move Done\n", 0);
 
 
-		linear_node.Status.AlertsClear();
-		linear_node.Motion.NodeStopClear();
-		linear_node.EnableReq(true);
-		printf("Node \t%i enabled\n", 0);
-
-
-		double timeout = cp_mgr.TimeStampMsec() + 2000;
-		while (!linear_node.Motion.IsReady()) {
-			if (cp_mgr.TimeStampMsec() > timeout) {
-				printf("Error: Timed out waiting for Node %d to enable\n", 0);
-				return -2;
-			}
-		}
-
-		linear_node.Motion.MoveWentDone();
-
-		linear_node.AccUnit(INode::RPM_PER_SEC);
-		linear_node.VelUnit(INode::RPM);
-		linear_node.Motion.AccLimit = ACC_LIM_RPM_PER_SEC;
-		linear_node.Motion.VelLimit = VEL_LIM_RPM;
-
-		printf("Moving Node \t%i \n", 0);
-		linear_node.Motion.MovePosnStart(MOVE_DISTANCE_CNTS);
-		printf("%f estiomated time.\n", linear_node.Motion.MovePosnDurationMsec(MOVE_DISTANCE_CNTS));
-		timeout = cp_mgr.TimeStampMsec() + linear_node.Motion.MovePosnDurationMsec(MOVE_DISTANCE_CNTS) + 100;
-
-		while (!linear_node.Motion.MoveIsDone()) {
-			if (cp_mgr.TimeStampMsec() > timeout) {
-				printf("Error: Timed out waiting for move to complete\n");
-				return -2;
-			}
-		}
-		printf("Node \t%i Move Done\n", 0);
-
-
-		linear_node.EnableReq(false);
+        // Disable motor
+        linear_node.EnableReq(false);
 
 
     }
-	catch (mnErr& theErr)
-	{
-		printf("Caught error: addr=%d, err=0x%08x\nmsg=%s\n", theErr.TheAddr, theErr.ErrorCode, theErr.ErrorMsg);
-		return -1;
+    catch (mnErr& theErr)
+    {
+        printf("Caught error: addr=%d, err=0x%08x\nmsg=%s\n", theErr.TheAddr, theErr.ErrorCode, theErr.ErrorMsg);
+        return -1;
 
     }
 
